2df1e42cdde543cdaf738efb73a253820484d3ac
nxvafps <oliverquirk11@gmail.com>
2025-01-22 00:15:14 +0000

    add custom error handling and global error handling

--- app/app.js
+++ app/app.js
@@ -6,4 +6,18 @@ const app = express();
 app.use(express.json());
 app.use("/api", apiRouter);
 
+app.all("*", (req, res, next) => {
+  next(new CustomError(404, `Can't find ${req.originalUrl} on this server`));
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
+  });
+});
 module.exports = app;
